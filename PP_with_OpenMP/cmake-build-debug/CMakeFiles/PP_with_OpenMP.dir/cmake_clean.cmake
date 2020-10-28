fac2938af85f873f884ce9a649d03dc4f689921b
BazovaLida <lydiaofficialv@gmail.com>
2020-10-28 16:30:51 +0200

    Transfering files

---
+++ PP_with_OpenMP/cmake-build-debug/CMakeFiles/PP_with_OpenMP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PP_with_OpenMP.dir/Data.cpp.o"
+  "CMakeFiles/PP_with_OpenMP.dir/main.cpp.o"
+  "PP_with_OpenMP"
+  "PP_with_OpenMP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PP_with_OpenMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
